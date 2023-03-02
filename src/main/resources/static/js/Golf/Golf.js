cd5dd7a833a19152d43bad8eb4d74233a469dcbb
V2seon <101914064+V2seon@users.noreply.github.com>
2023-03-02 15:43:45 +0900

    0302 이선재

---
+++ src/main/resources/static/js/Golf/Golf.js
@@ -0,0 +1,19 @@
+function sub(){
+const ccname = document.getElementById("cc_name").value;
+const year = document.getElementById("year").value;
+const month = document.getElementById("month").value;
+const day = document.getElementById("day").value;
+const t1 = document.getElementById("t1").value;
+const t2 = document.getElementById("t2").value;
+const c = document.getElementById("c").value;
+const cc = document.getElementById("cc").value;
+
+console.log(ccname);
+console.log(year);
+console.log(month);
+console.log(day);
+console.log(t1);
+console.log(t2);
+console.log(c);
+console.log(cc);
+}
